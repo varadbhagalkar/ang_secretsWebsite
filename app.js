7ffcfaddec1938bc2ffc1005636e7987a498bf31
viftergr <varadbhagalkar@gmail.com>
2022-09-24 20:40:40 +0530

    bcrypt

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const app = express();
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption')
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -19,9 +20,6 @@ const userSchema = new mongoose.Schema({
     password: { type: String, required: true },
 });
 
-
-
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] });
 
 const User = mongoose.model("User", userSchema);
 
@@ -38,40 +36,43 @@ app.get("/register", function (req, res) {
 });
 
 app.post("/register", function (req, res) {
-    const newUser = new User({
-        email: req.body.username,
-        password: req.body.password
+    bcrypt.hash(myPlaintextPassword, saltRounds, function (err, hash) {
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+        });
+
+        newUser.save(function (err) {
+            if (err) {
+                console.log(err);
+            } else {
+                res.render('secrets');
+            }
+
+        });
     });
 
-    newUser.save(function (err) {
-        if (err) {
-            console.log(err);
-        } else {
-            res.render('secrets');
-        }
-
-    });
 });
 
 app.post("/login", function (req, res) {
     const username = req.body.username;
-    const password = req.body.password;
+    const password =
 
-    User.findOne({ email: username }, function (err, foundUser) {
-        if (err) {
-            console.log(err);
-        } else {
-            console.log(foundUser)
-            if (foundUser) {
-                if (foundUsers.password === password) {
-                    res.render('secrets');
-                } else {
-                    res.send("Wrong Password")
+        User.findOne({ email: username }, function (err, foundUser) {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log(foundUser)
+                if (foundUser) {
+                    bcrypt.compare(password, foundUser.password, function (err, result) {
+                        if (result) {
+                            res.render('secrets')
+                        }
+                    });
                 }
-            }
 
-        }
-    });
+            }
+        });
 
 });
 
